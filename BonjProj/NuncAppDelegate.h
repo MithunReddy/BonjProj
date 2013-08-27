12053e0ff77e6390e75528501b88da88a1f0bcab
mithun07 <mithun.karampudi@nuncsystems.com>
2013-08-27 18:38:49 +0530

    Initial Commit

---
+++ BonjProj/NuncAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  NuncAppDelegate.h
+//  BonjProj
+//
+//  Created by nunc03 on 8/27/13.
+//  Copyright (c) 2013 nunc03. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface NuncAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ BonjProj/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  BonjProj
+//
+//  Created by nunc03 on 8/27/13.
+//  Copyright (c) 2013 nunc03. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "NuncAppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([NuncAppDelegate class]));
+    }
+}
